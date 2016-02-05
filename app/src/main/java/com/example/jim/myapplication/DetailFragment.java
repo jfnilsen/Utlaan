5170e66ac40f23daaf760b45c6ed2aa855a0f97c
Jim Frode Nilsen <jimnil93@hotmail.com>
2016-02-05 13:46:11 +0100

    As the gson didn't include a bitarray for the images I implemented a way to download the images from their url

--- app/src/main/java/com/example/jim/myapplication/DetailFragment.java
+++ app/src/main/java/com/example/jim/myapplication/DetailFragment.java
@@ -1,20 +1,32 @@
 package com.example.jim.myapplication;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
  * Created by Jim on 04/02/2016.
  */
 public class DetailFragment extends Fragment {
+
+    Bitmap bitmap;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,6 +54,40 @@ public class DetailFragment extends Fragment {
         ((TextView)detailView.findViewById(R.id.imageurl)).setText(equipment.getImage_url());
 
         //TODO: Display the image in the fragment
+        ImageView image = (ImageView)detailView.findViewById(R.id.image);
+        runImageThread(image, equipment.getImage_url());
+        image.setImageBitmap(bitmap);
 
+
+    }
+
+    private void runImageThread(final ImageView viewById, final String image_url) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ImageView image = viewById;
+                bitmap = getBitmap(image_url);
+
+            }
+        });
+
+        try {
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap getBitmap(String url) {
+        Bitmap bitmap = null;
+
+        try {
+            URL img_Url = new URL(url);
+            bitmap = BitmapFactory.decodeStream(img_Url.openConnection().getInputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
